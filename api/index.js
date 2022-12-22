0be8710bba1f759cb68b2deda3728211ad685e9d
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-12-22 13:07:19 +0300

    Edits & routes

---
+++ api/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
+const app = express()
+dotenv.config()
+mongoose.set('strictQuery', false); //For terminal error
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB")
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoDB disconnected!")
+})
+mongoose.connection.on("connected", ()=>{
+    console.log("mongoDB connected!")
+})
+
+
+//middlewares
+app.use("/api/auth",authRoute);
+app.use("/api/users",usersRoute);
+app.use("/api/hotels",hotelsRoute);
+app.use("/api/rooms",roomsRoute);
+
+app.listen(8800, () => {
+    connect()
+    console.log("Connected to backend")
+});
--- index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import dotenv from "dotenv";
-const app = express()
-dotenv.config()
-
-app.listen(8800,()=>{
-    console.log("Connected to backend")
-});
