1e92606c1c10a8d90a1121873a3316fbdb714b32
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-12-24 00:23:43 +0300

    Role-based authorization

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,17 +1,18 @@
 import express from "express";
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js"
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 //Create
-router.post("/",createHotel);
+router.post("/",verifyAdmin,createHotel);
 
 //Update
-router.put("/:id",updateHotel);
+router.put("/:id",verifyAdmin,updateHotel);
 
 //Delete
-router.delete("/:id",deleteHotel);
+router.delete("/:id",verifyAdmin,deleteHotel);
 
 //Get
 router.get("/:id",getHotel);
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,20 +1,32 @@
 import express from "express";
 import { updateUser,deleteUser,getUser,getUsers} from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// router.get("/checkauthentication",verifyToken,(req,res,next)=>{
+//     res.send("Hello user, you are logged in")
+// });
+
+// router.get("/checkuser/:id",verifyUser,(req,res,next)=>{
+//     res.send("Hello user, you are logged in and you can delete you account")
+// });
+
+// router.get("/checkadmin/:id",verifyAdmin,(req,res,next)=>{
+//     res.send("Hello admin, you are logged in and you can delete all accounts")
+// });
 
 //Update
-router.put("/:id",updateUser);
+router.put("/:id",verifyUser,updateUser);
 
 //Delete
-router.delete("/:id",deleteUser);
+router.delete("/:id",verifyUser,deleteUser);
 
 //Get
-router.get("/:id",getUser);
+router.get("/:id",verifyUser,getUser);
 
 //Get all
-router.get("/",getUsers);
+router.get("/",verifyAdmin,getUsers);
 
 
 export default router
