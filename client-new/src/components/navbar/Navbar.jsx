5703243ec5cff896281d7ed84f44abffe321b7c4
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-12-25 12:39:53 +0300

    home link

--- client-new/src/components/navbar/Navbar.jsx
+++ client-new/src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import "./navbar.css"
-
+import {Link} from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">lamabooking</span>
+        <Link to="/" style={{color:"inherit",textDecoration:"none"}}>
+          <span className="logo">lamabooking</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
