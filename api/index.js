f8ac96d4dcc43d505a04cbbaa80f94347de08e20
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-12-25 12:03:09 +0300

    cors install

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import usersRoute from "./routes/users.js"
 import hotelsRoute from "./routes/hotels.js"
 import roomsRoute from "./routes/rooms.js"
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
 const app = express()
@@ -29,7 +30,7 @@ mongoose.connection.on("connected", () => {
 })
 
 //middlewares
-
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
