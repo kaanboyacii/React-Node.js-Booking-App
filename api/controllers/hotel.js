0aed29d699a44690ffb37c6b99911cc3391ca755
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-12-27 13:29:11 +0300

    Reservation Component setup 1

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -84,3 +84,16 @@ export const countByType = async (req, res, next) => {
         next(err);
     }
 };
+export const getHotelRooms = async (req, res, next) => {
+    try {
+      const hotel = await Hotel.findById(req.params.id);
+      const list = await Promise.all(
+        hotel.rooms.map((room) => {
+          return Room.findById(room);
+        })
+      );
+      res.status(200).json(list)
+    } catch (err) {
+      next(err);
+    }
+  };
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotelRooms, getHotels, updateHotel } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js"
 import { verifyAdmin } from "../utils/verifyToken.js";
 
@@ -21,6 +21,7 @@ router.get("/find/:id",getHotel);
 router.get("/",getHotels);
 router.get("/countByCity",countByCity);
 router.get("/countByType",countByType);
+router.get("/room/:id",getHotelRooms);
 
 
 
