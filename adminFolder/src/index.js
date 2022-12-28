d7c8de5a1cb14c2eb25c0071a4887087d89faca4
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-12-28 14:22:38 +0300

    AuthContext import admin & install axios

--- adminFolder/src/index.js
+++ adminFolder/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { DarkModeContextProvider } from "./context/darkModeContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <DarkModeContextProvider>
-      <App />
-    </DarkModeContextProvider>
+    <AuthContextProvider>
+      <DarkModeContextProvider>
+        <App />
+      </DarkModeContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
